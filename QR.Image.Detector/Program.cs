4e659b1aff1e38fa652dd77524192e3ee4e1a7ea
Juan Ramos <ptjuanramos@gmail.com>
2021-01-09 20:26:51 +0000

    init commit

---
+++ QR.Image.Detector/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QR.Image.Detector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
